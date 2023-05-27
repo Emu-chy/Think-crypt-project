2543ccc13dc27134842a90f0d4b672656515af9f
Emon Chowdhury <emonchowdhury942858@gmail.com>
2023-05-28 02:18:44 +0600

    initial commit

---
+++ src/app/components/Herosection.js
@@ -0,0 +1,54 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Herosection = () => {
+    return (
+        <>
+            {/* <section className="text-white py-24">
+                <div className="container mx-auto text-center">
+                    
+                </div>
+            </section> */}
+            <section>
+                <div className="">
+                    <img src="/Hero.png" alt="banner" className="w-full image-height" />
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Herosection;
+
+{
+    /* <div class="min-h-screen bg-right-bottom bg-cover flex">
+<div class="relative container mx-auto p-4 flex items-end z-10">
+    <div>
+        <div class="">
+            <img src="/Hero.png" alt="banner" className="h-100 w-100" />
+        </div>
+    </div>
+</div>
+</div> */
+}
+
+{
+    /* <h1 className="text-5xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500">
+                    Welcome to My Site!
+                </h1>
+                <p className="text-gray-700 text-lg mb-8">Browse around for amazing content!</p>
+                <div className="space-x-2">
+                    <a
+                        href="#"
+                        className="py-3 px-8 bg-gray-400 hover:bg-gray-300 text-gray-800 hover:text-gray-900 rounded-lg hover:shadow-xl transition duration-300"
+                    >
+                        Learn More
+                    </a>
+                    <a
+                        href="#"
+                        className="py-3 px-8 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 rounded-lg hover:shadow-xl transition duration-300"
+                    >
+                        Buy Now
+                    </a>
+                </div> */
+}
---
+++ src/app/components/Story-content.js
@@ -0,0 +1,76 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const StoryContent = () => {
+    return (
+        <>
+            {/* <div className="flex mt-10">
+                <div className="mx-24 w-96">
+                    <img src="/Frame3.png" alt="frame" className="min-w-full" />
+                </div>
+                <div className="mx-24">
+                    <p className="w-50">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum,
+                        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                        explicabo.{" "}
+                    </p>
+                </div>
+            </div> */}
+
+            <main class="mt-32 flex flex-col xl:flex-row">
+                <div class="mx-24 h-full">
+                    <img
+                        class="object-cover w-full frame-img"
+                        src="/Frame3.png"
+                        alt="Robot Group"
+                    />
+                </div>
+
+                <div class="w-full xl:w-1/2  h-screen flex  justify-center">
+                    <div>
+                        <h1 class="heading1 my-5 ">
+                            The Story of Vincent’s <br />
+                            Sphere
+                        </h1>
+                        <p class="mt-5 paragraph">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                            mollit anim id est laborum, illo inventore veritatis et quasi architecto
+                            beatae vitae dicta sunt explicabo.
+                        </p>
+                        <p class="mt-4 paragraph">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+                            illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                            explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+                            odit aut fugit, sit amet, consectetur, adipisci velit, sed quia non
+                            numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
+                            quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+                            exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+                            commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+                            voluptate velit esse quam illo inventore veritatis et quasi architecto
+                            beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia nihil
+                            molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                            nulla pariatur?
+                        </p>
+
+                        <button class=" mt-12 inline-block bg-black px-4 py-2 text-white">
+                            Learn More
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default StoryContent;
